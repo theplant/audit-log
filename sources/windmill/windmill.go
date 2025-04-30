bf6864eef859b49a9128e0f0d94bb1cdb718fde3
Bodhi <bodhi@theplant.jp>
2025-04-30 15:48:34 +1000

    Make windmill.FetchAndUpdateState return a string instead of []byte

--- sources/windmill/windmill.go
+++ sources/windmill/windmill.go
@@ -11,17 +11,17 @@ import (
 )
 
 // FetchAndUpdateState handles the common Windmill logic for fetching logs and managing state
-func FetchAndUpdateState(fetcher schema.LogFetcher) ([]byte, error) {
+func FetchAndUpdateState(fetcher schema.LogFetcher) (string, error) {
 	from := time.Now().Add(-time.Hour * 1)
 
 	lastSeen, err := wmill.GetState()
 	if err != nil {
-		return nil, fmt.Errorf("failed to get windmill state: %w", err)
+		return "", fmt.Errorf("failed to get windmill state: %w", err)
 	}
 	if lastSeen != "" {
 		from, err = time.Parse(time.RFC3339Nano, lastSeen.(string))
 		if err != nil {
-			return nil, fmt.Errorf("failed to parse last seen %s: %w", lastSeen, err)
+			return "", fmt.Errorf("failed to parse last seen %s: %w", lastSeen, err)
 		}
 	}
 
@@ -29,14 +29,15 @@ func FetchAndUpdateState(fetcher schema.LogFetcher) ([]byte, error) {
 
 	logs, err := fetcher.FetchLogs(context.Background(), from, to)
 	if err != nil {
-		return nil, fmt.Errorf("failed to fetch logs: %w", err)
+		return "", fmt.Errorf("failed to fetch logs: %w", err)
 	}
 
 	if len(logs) > 0 {
 		wmill.SetState(logs[0].Timestamp.Format(time.RFC3339Nano))
 	}
 
-	return json.Marshal(logs)
+	str, err := json.Marshal(logs)
+	return string(str), err
 }
 
 // import (
